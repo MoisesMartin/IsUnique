991519a5fde2c47293ca5a20672a4ae5175a3aa0
MoisesMartin <moises01@live.com.mx>
2021-05-17 19:36:26 -0500

    2nd commit

--- src/com/moises/Main.java
+++ src/com/moises/Main.java
@@ -1,7 +1,10 @@
 package com.moises;
 
 public class Main {
+/*Bool Is Unique(String iu) -> Implement an algorithm to determine if a string has all Unique characters.
+  do not use any predefined data structures*/
 
+    public String userInput="";
     public static void main(String[] args) {
 	// write your code here
     }
